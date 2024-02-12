027bea1646e3aa73a2bd3e4707e2a9ba351e55aa
MohseenHabib <raonerama8@gmail.com>
2024-02-12 09:45:48 +0300

    Building UI

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,31 +1,48 @@
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet, Image } from 'react-native';
+import Colors from '@/src/constants/Colors';
 import EditScreenInfo from '@/src/components/EditScreenInfo';
 import { Text, View } from '@/src/components/Themed';
-
+import products from '@/assets/data/products';
+const product = products[0];
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Mohnhein</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Image source={{ uri: product.image }} style= {styles.image} />
+      <Text style= {styles.title}>{product.name}</Text>
+      <Text style={styles.price}>SAR {product.price}</Text>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    padding: 10,
+    margin: 0,
+    borderRadius: 20
+    
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1,
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginVertical: 5 ,
+    color: 'black'
+    
   },
   separator: {
-    marginVertical: 30,
+    marginVertical: 20,
     height: 1,
     width: '80%',
   },
+  price: {
+    fontStyle: 'italic',
+    fontWeight: '500',
+    color: '#2f95dc'
+  },
+  
 });
