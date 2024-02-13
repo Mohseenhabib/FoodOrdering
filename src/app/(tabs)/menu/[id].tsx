caf45489d88ec63399d39f79170e1a8edf0e2d4f
MohseenHabib <raonerama8@gmail.com>
2024-02-13 14:08:38 +0300

    Products Detail Page

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,17 +1,76 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet, Pressable } from 'react-native'
 import React from 'react'
 import { Stack, useLocalSearchParams } from 'expo-router'
+import products from '@/assets/data/products';
+import { useState } from 'react';
+import Button from '@/src/components/Button';
+
+const sizes = ['S','M', 'L', 'XL'];
 
 const ProductDetailsScreen = () => {
   const {id} = useLocalSearchParams();
-  const {name} = useLocalSearchParams();
+  const[selectedSize, setselectedSize]= useState('L');
+  const product = products.find((p) => p.id.toString() === id);
+  const addToCart= ()=>{
+    console.warn('Adding To cart');
+  }
   return (
    
-    <View>
-      <Stack.Screen options={{title: "Details: " + id}}/>
-      <Text style= {{ color: "white"}}>ProductDetailsScreen : {id}</Text>
+    <View style= {styles.container}>
+      <Stack.Screen options={{title: product?.name}}/>
+      <Image source= {{uri: product.image}} style= {styles.image}/>
+      <Text style={styles.sizeText}>Select Size</Text>
+      <View style={styles.sizes}>
+      {sizes.map((size)=>(
+       <Pressable onPress= {() => {setselectedSize(size);}} style={[styles.size,{backgroundColor:selectedSize == size ? 'red': 'black'}]} key={size}>
+        <Text style={[styles.sizeText,{color:selectedSize == size? 'black': 'white'}]}>{size}</Text>
+      </Pressable>
+      ))}
+      </View>
+      <Text style= {styles.price}>SAR {product.price}</Text>
+      <Button onPress={addToCart} text='Add To Cart'/>
     </View>
   );
 };
+const styles=StyleSheet.create({
+container:{
+  backgroundColor: 'black',
+  padding: 10,
+  flex: 1,
+},
+image:{
+  width: '100%',
+  aspectRatio : 1,
+  
+},
+price:{
+  fontSize: 18,
+  fontWeight: 'bold',
+  color: 'white',
+  padding:5,
+  marginTop: 'auto'
+},
+sizeText: {
+color: 'white',
+fontSize: 15,
+textAlign: 'center',
+padding:15,
+marginTop: 'auto'
+},
+sizes: {
+flexDirection: 'row',
+justifyContent: 'space-around',
+fontSize: 20,
+padding: 10,
+},
+size: { 
+backgroundColor: 'red',
+width: 50,
+aspectRatio: 1,
+borderRadius: 25,
+alignItems: 'center',
+justifyContent:'center',
 
+}
+});
 export default ProductDetailsScreen;
